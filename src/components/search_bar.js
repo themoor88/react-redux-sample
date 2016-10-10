17e8488578b72de425ec08efc3d151b4546aa54f
Wajid Kagzi <wjkagzi@gmail.com>
2016-10-10 11:19:59 -0400

    Add initial state using constructor and add onchange event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const SearchBar = () => {
-  return <input />;
-};
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''}
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={(e) => this.setState({term: e.target.value})} />
+      </div>
+    );
+  }
+}
 
 export default SearchBar;

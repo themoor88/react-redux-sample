9a15db5dccd393c08b0e603e47adbd60878bdf65
Wajid Kagzi <wjkagzi@gmail.com>
2016-10-13 23:13:29 -0400

    Render video list items in the video list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
+import VideoListItem from './video_list_item';
+
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+
   return (
-    <ul className='col-md-4 list-group' >
-      {props.videos.length}
+    <ul className='col-md-4 list-group'>
+      {videoItems}
     </ul>
   )
-}
+};
 
 export default VideoList;
---
+++ src/components/video_list_item.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const VideoListItem = (props) => {
+  return <li>{video}</li>
+};
+
+export default VideoListItem;

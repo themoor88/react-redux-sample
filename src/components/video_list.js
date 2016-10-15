8d33d0e475f379c048bf46551fd4df91548bc8f3
Wajid Kagzi <wjkagzi@gmail.com>
2016-10-14 22:37:47 -0400

    Generate content from video search for video list item

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} />
+    return <VideoListItem key={video.etag} video={video} />
   });
 
   return (
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-const VideoListItem = (props) => {
-  return <li>{video}</li>
+const VideoListItem = ({video}) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
+        </div>
+      </div>
+    </li>
+  )
 };
 
 export default VideoListItem;

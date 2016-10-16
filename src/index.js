6cf56aac2eaa7be712708653f28986b6cc4707b0
Wajid Kagzi <wjkagzi@gmail.com>
2016-10-15 23:16:19 -0400

    Passing functions as props down from parent to update video onclick

--- src/index.js
+++ src/index.js
@@ -12,11 +12,15 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
-      this.setState({videos});
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -24,8 +28,8 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} onVideoSelect={selectedVideo => this.setState({selectedVideo})} />
       </div>
     )
   }
